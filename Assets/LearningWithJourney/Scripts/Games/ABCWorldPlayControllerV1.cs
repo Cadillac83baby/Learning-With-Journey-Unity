86a5b96d126fb447a561594f2405bf608b8552ab
Cadillac83baby <d.s.h.ment@gmail.com>
2026-09-08 23:56:16 -0700

    Route ABC Back button through guarded menu load

--- Assets/LearningWithJourney/Scripts/Games/ABCWorldPlayControllerV1.cs
+++ Assets/LearningWithJourney/Scripts/Games/ABCWorldPlayControllerV1.cs
@@ -102,7 +102,7 @@ namespace LearningWithJourney.Games
                 GameProgressService.Instance.OnProgressChanged -= RefreshPoints;
         }
 
-        public void GoHome() => SceneManager.LoadScene("MainMenu");
+        public void GoHome() => SceneRouter.LoadMainMenu();
 
         public void ResetABCProgress()
         {
